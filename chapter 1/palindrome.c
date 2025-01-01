20ad641c81f5274ef35c1bbbe7d7b508e3cdedd0
Sonu <100nu0002@gmail.com>
2025-01-01 20:28:24 +0530

    palindrome

---
+++ chapter 1/palindrome.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+   int main(){
+   int n, rem, rvrs =0, temp;
+   printf("enter the number :");
+   scanf("%d" , &n);
+   temp =n;
+   while (n !=0){
+    rem = n % 10;
+    rvrs = rvrs * 10+rem;
+    n /= 10; 
+   }
+   if (temp == rvrs)
+   {
+    printf ("%d is a palindrome number", temp);  
+   }
+     else
+     printf("it is not a palindrome");
+
+     return 0;
+   }
